187d04aa93731a6c850512b12aa0e97e4e407fa7
nikshrma <nikshrma2006@gmail.com>
2026-07-18 23:57:28 +0530

    added runtime config parsing

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,11 +6,13 @@ import (
 	"net/url"
 
 	"github.com/nikshrma/heimdall/internal/balancer"
+	"github.com/nikshrma/heimdall/internal/config"
+	"github.com/rs/zerolog/log"
 )
 
 type Backend struct {
 	URL   *url.URL
-	proxy httputil.ReverseProxy
+	Proxy *httputil.ReverseProxy
 }
 type Routes struct {
 	Path        string
@@ -18,4 +20,31 @@ type Routes struct {
 	StripPrefix bool
 	bala        balancer.Balancer
 	backends    []Backend
+}
+
+func buildBackends(backends []string) []*Backend {
+	var runtimeBackends []*Backend
+	for _, be := range backends {
+		target, err := url.Parse(be)
+		if err != nil {
+			log.Fatal().Err(err).Msg("invalid backend url")
+		}
+		proxy := &httputil.ReverseProxy{
+			Rewrite: func(pr *httputil.ProxyRequest) {
+				pr.SetURL(target)
+				pr.SetXForwarded()
+			},
+		}
+		runtimeBackends = append(runtimeBackends, &Backend{
+			URL:   target,
+			Proxy: proxy,
+		})
+	}
+	return runtimeBackends
+}
+
+func Build(cfg config.Config) ([]*Routes, error) {
+	for _, rc := range cfg.Routes {
+		backends := buildBackends(rc.Backends)
+	}
 }
