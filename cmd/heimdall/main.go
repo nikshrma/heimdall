4a15394a6ec63b6e1f193416321f52120a79059c
nikshrma <nikshrma2006@gmail.com>
2026-07-21 23:47:44 +0530

    feat(gateway): add dispatch layer wiring router, balancer, and backend proxy

--- cmd/heimdall/main.go
+++ cmd/heimdall/main.go
@@ -5,26 +5,39 @@ import (
 	"net/http"
 
 	"github.com/nikshrma/heimdall/internal/config"
+	"github.com/nikshrma/heimdall/internal/gateway"
+	"github.com/nikshrma/heimdall/internal/router"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	// Load config
 	cfg, err := config.Load("configs/routes.yml")
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to read config file")
 	}
+
+	// set global log level
 	level, err := zerolog.ParseLevel(cfg.Log.Level)
 	if err != nil {
 		log.Fatal().Err(err).Msg("invalid log level")
 	}
 	zerolog.SetGlobalLevel(level)
 	log.Debug().Interface("config", cfg).Msg("loaded config")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	})
-	log.Info().Msg("starting server")
 
+	// load dynamic routes
+	routes, err := router.Build(*cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to build routes")
+	}
+
+	// Use gateway
+	gw := gateway.New(routes)
+	mux := http.NewServeMux()
+	mux.Handle("/", gw)
+
+	log.Info().Msg("starting server")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal().Err(err).Msg("server failed")
 	}
--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -1,2 +1,45 @@
 // Package gateway is the main dispatch package
 package gateway
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/nikshrma/heimdall/internal/router"
+	"github.com/rs/zerolog/log"
+)
+
+type Gateway struct {
+	routes []*router.Route
+}
+
+func New(routes []*router.Route) *Gateway {
+	return &Gateway{
+		routes: routes,
+	}
+}
+
+func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	route, err := router.Match(g.routes, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusMethodNotAllowed)
+	}
+	if route == nil {
+		http.NotFound(w, r)
+	}
+	b := route.Balancer.Next()
+	if b == nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+	}
+	if route.StripPrefix == true {
+		r.URL.Path = strings.TrimPrefix(r.URL.Path, route.Path)
+		if r.URL.Path == "" {
+			r.URL.Path = "/"
+		}
+	}
+	log.Info().
+		Str("url", r.URL.Path).
+		Str("backend", b.URL.String()).
+		Msg("dispatching request")
+	b.Proxy.ServeHTTP(w, r)
+}
