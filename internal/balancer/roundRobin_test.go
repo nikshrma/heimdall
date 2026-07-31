9733f5dabb28a260f6e113105f9b126909b205b7
nikshrma <nikshrma2006@gmail.com>
2026-08-01 00:13:27 +0530

    balancer tests

--- internal/balancer/roundRobin_test.go
+++ internal/balancer/roundRobin_test.go
@@ -16,4 +16,20 @@ func TestRoundRobinNext(t *testing.T) {
 	if rr.Next() != backends[0] {
 		t.Error("expected first backend")
 	}
+	if rr.Next() != backends[1] {
+		t.Error("expected second backend")
+	}
+	if rr.Next() != backends[2] {
+		t.Error("expected third backend")
+	}
+	if rr.Next() != backends[0] {
+		t.Error("expected first backend again")
+	}
+}
+
+func TestRoundRobinEmpty(t *testing.T) {
+	rr := NewRoundRobin(nil)
+	if rr.Next() != nil {
+		t.Error("expected nil backend")
+	}
 }
