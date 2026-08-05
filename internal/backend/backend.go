aeda61e0e99d7818c6c984c205788c07cab259ab
nikshrma <nikshrma2006@gmail.com>
2026-08-05 23:13:23 +0530

    added health check funcs

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -4,11 +4,23 @@ package backend
 import (
 	"net/http/httputil"
 	"net/url"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/nikshrma/heimdall/internal/health"
 )
 
 type Backend struct {
 	URL   *url.URL
 	Proxy *httputil.ReverseProxy
+
+	mu      sync.RWMutex
+	polling atomic.Bool
+
+	healthy      bool
+	failureCount atomic.Int32
+	successCount atomic.Int32
 }
 
 func New(be string) (*Backend, error) {
@@ -26,4 +38,48 @@ func New(be string) (*Backend, error) {
 		Proxy: proxy,
 		URL:   target,
 	}, nil
+}
+
+func IsHealthy(b *Backend) bool {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return b.healthy
+}
+
+func MarkSuccess(b *Backend) {
+	b.failureCount.Store(0)
+	b.successCount.Add(1)
+	if b.successCount.Load() >= 3 {
+		b.healthy = true
+	}
+}
+
+func MarkFailure(b *Backend) {
+	b.successCount.Store(0)
+	b.failureCount.Add(1)
+	if b.failureCount.Load() >= 3 {
+		b.healthy = false
+		if b.polling.CompareAndSwap(false, true) {
+			go StartPolling(b)
+		}
+		defer b.polling.Store(false)
+	}
+}
+
+func StartPolling(b *Backend) {
+	defer b.polling.Store(false)
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+		if health.CheckHealth(b) {
+			MarkSuccess(b)
+			if IsHealthy(b) {
+				return
+			}
+		} else {
+			MarkSuccess(b)
+		}
+	}
 }
