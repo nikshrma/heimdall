e837ac7e02f2049caff4557279a4329d55350a3c
nikshrma <nikshrma2006@gmail.com>
2026-08-18 23:05:03 +0530

    feat: policy methods

--- internal/retry/policy.go
+++ internal/retry/policy.go
@@ -6,6 +6,17 @@ type Policy struct {
 	MaxAttempts int32
 }
 
+func NewPolicy() *Policy {
+	return &Policy{
+		MaxAttempts: 2,
+	}
+}
+
+func (p *Policy) AttemptAgain() bool {
+	p.MaxAttempts--
+	return p.MaxAttempts > 0
+}
+
 func ShouldRetryMethod(method string) bool {
 	if method == http.MethodGet || method == http.MethodHead || method == http.MethodOptions {
 		return true
