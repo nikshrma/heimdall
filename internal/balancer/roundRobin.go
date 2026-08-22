54aed93030227eb8e5c7f0b0d87fe1e140fc22b9
nikshrma <nikshrma2006@gmail.com>
2026-08-22 12:52:00 +0530

    fix: rr punches one ticket/req

--- internal/balancer/roundRobin.go
+++ internal/balancer/roundRobin.go
@@ -18,13 +18,13 @@ func NewRoundRobin(backends []*backend.Backend) Balancer {
 }
 
 func (rr *roundRobin) Next(excluded map[*backend.Backend]struct{}) *backend.Backend {
-	if len(rr.backends) == 0 {
+	n := len(rr.backends)
+	if n == 0 {
 		return nil
 	}
-	n := uint64(len(rr.backends))
 	start := rr.counter.Add(1) - 1
-	for i := uint64(0); i < n; i++ {
-		ind := (start + i) % n
+	for i := 0; i < n; i++ {
+		ind := (start + uint64(i)) % uint64(n)
 		b := rr.backends[ind]
 		if _, ok := excluded[b]; ok {
 			continue
