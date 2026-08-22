acf5038b890f57d7b1c66da70954187e17c8ba39
nikshrma <nikshrma2006@gmail.com>
2026-08-22 12:51:19 +0530

    refactor: structured zerolog logging

--- internal/backend/transport.go
+++ internal/backend/transport.go
@@ -3,6 +3,8 @@ package backend
 import (
 	"net/http"
 	"time"
+
+	"github.com/rs/zerolog/log"
 )
 
 type breakerTransport struct {
@@ -12,15 +14,44 @@ type breakerTransport struct {
 
 func (t *breakerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	start := time.Now()
+
 	resp, err := t.next.RoundTrip(req)
 	duration := time.Since(start)
-	if err != nil || resp != nil && resp.StatusCode >= 500 || duration > t.b.slowThreshold {
+
+	switch {
+	case err != nil:
+		log.Info().
+			Str("backend", t.b.URL().String()).
+			Err(err).
+			Dur("duration", duration).
+			Msg("breaker: fail (transport error)")
+
 		t.b.MarkFailure()
-	} else {
+
+	case resp != nil && resp.StatusCode >= 500:
+		log.Info().
+			Str("backend", t.b.URL().String()).
+			Int("status", resp.StatusCode).
+			Dur("duration", duration).
+			Msg("breaker: fail (5xx)")
+
+		t.b.MarkFailure()
+
+	case duration > t.b.slowThreshold:
+		log.Info().
+			Str("backend", t.b.URL().String()).
+			Dur("duration", duration).
+			Msg("breaker: fail (slow)")
+
+		t.b.MarkFailure()
+
+	default:
 		t.b.MarkSuccess()
 	}
+
 	if t.b.state.Load() == int32(HalfOpen) {
 		t.b.trialInFlight.Store(false)
 	}
+
 	return resp, err
 }
